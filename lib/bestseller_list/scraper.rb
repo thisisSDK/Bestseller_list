ce17e93fc054ffe8ab28f6b50a73b710a4f38948
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2018-11-17 12:39:58 -0500

    attempts iteration over the book elements

--- lib/bestseller_list/scraper.rb
+++ lib/bestseller_list/scraper.rb
@@ -4,5 +4,23 @@ class BestsellerList::Scraper
     html = open("https://www.goodreads.com/choiceawards/best-food-cookbooks-2018")
     doc = Nokogiri::HTML(html)
     binding.pry
+
   end
 end
+
+
+#####################
+####best practices###
+#####################
+#separation of concenrs
+#making comments
+#name spacing
+#revise after the fact
+#commit often
+# doc.css(".pollContents").css(".answerWrapper").css(".js-tooltipTrigger").each do |book_element|
+#   binding.pry
+#
+# end
+
+#other possibility
+#doc.css(".pollContents").css(".pollAnswer").css(".tooltipTrigger").css(".pollAnswer__bookLink")
